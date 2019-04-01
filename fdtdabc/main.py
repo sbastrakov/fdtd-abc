dd3373ae1e4df067bd8c55f60af6f627424a9eef
Sergei Bastrakov <sergey.bastrakov@gmail.com>
2019-04-01 15:01:49 +0200

    Separate plotting into a new module

--- fdtdabc/main.py
+++ fdtdabc/main.py
@@ -1,12 +1,11 @@
 import fdtd.solver as fdtd
 import grid.yee
 from output.energy import Printer as EnergyPrinter
+from output.plot import Plot
 import pml.solver.convolutional as cpml
 import pml.solver.split_field as sfpml
 
 import math
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 import numpy as np
 import scipy.constants
 
@@ -28,38 +27,6 @@ def init_grid(solver):
     num_guard_cells_left, num_guard_cells_right = np.array(solver.get_guard_size(num_internal_cells)) # do not modify this
     gr = grid.yee.Yee_grid(min_position, max_position, num_internal_cells, num_guard_cells_left, num_guard_cells_right)
     return gr
-
-class Plot:
-    def __init__(self, period):
-        self.period = period
-        #self.fig, self.axs = plt.subplots(1, 2, constrained_layout=True)
-        #self.axs[0].set_title('Ey(x, y, z_center)')
-        #self.axs[0].set_xlabel('x (cells)')
-        #self.axs[0].set_ylabel('y (cells)')
-        #self.axs[1].set_title('Bz(x, y, z_center)')
-        #self.axs[1].set_xlabel('x (cells)')
-        #self.axs[1].set_ylabel('y (cells)')
-        self.fig = plt.figure()
-        self.images = []
-
-    def add_frame(self, grid, iteration):
-        if (self.period == 0) or (iteration % self.period != 0):
-            return
-        slice_z = grid.num_cells[2] // 2
-        #ey = np.transpose(grid.ey[:, :, slice_z])
-        #bz = np.transpose(grid.bz[:, :, slice_z])
-        #self.fig.suptitle('Iteration ' + str(iteration), fontsize=16)
-        #self.images.append(self.axs[0].imshow(ey))
-        #self.axs[1].imshow(bz)
-        ez = np.transpose(grid.ez[:, :, slice_z])
-        self.images.append([plt.imshow(ez)])
-
-    def animate(self):
-        if self.period == 0:
-            return
-        ani = animation.ArtistAnimation(self.fig, self.images, interval=500, blit=True,
-                                repeat_delay=1000)
-        plt.show()
 
 
 def add_source(grid, iteration, dt):
---
+++ fdtdabc/output/plot.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
+
+
+class Plot:
+    def __init__(self, period):
+        self.period = period
+        #self.fig, self.axs = plt.subplots(1, 2, constrained_layout=True)
+        #self.axs[0].set_title('Ey(x, y, z_center)')
+        #self.axs[0].set_xlabel('x (cells)')
+        #self.axs[0].set_ylabel('y (cells)')
+        #self.axs[1].set_title('Bz(x, y, z_center)')
+        #self.axs[1].set_xlabel('x (cells)')
+        #self.axs[1].set_ylabel('y (cells)')
+        self.fig = plt.figure()
+        self.images = []
+
+    def add_frame(self, grid, iteration):
+        if (self.period == 0) or (iteration % self.period != 0):
+            return
+        slice_z = grid.num_cells[2] // 2
+        #ey = np.transpose(grid.ey[:, :, slice_z])
+        #bz = np.transpose(grid.bz[:, :, slice_z])
+        #self.fig.suptitle('Iteration ' + str(iteration), fontsize=16)
+        #self.images.append(self.axs[0].imshow(ey))
+        #self.axs[1].imshow(bz)
+        ez = np.transpose(grid.ez[:, :, slice_z])
+        self.images.append([plt.imshow(ez)])
+
+    def animate(self):
+        if self.period == 0:
+            return
+        ani = animation.ArtistAnimation(self.fig, self.images, interval=500, blit=True,
+                                repeat_delay=1000)
+        plt.show()
